ac4c5c493aaabc9b266581746616245d74e75260
Deepak Pawar <pawardeepakv@gmail.com>
2020-01-29 18:58:46 +0530

    Initial commit

---
+++ app/src/main/java/com/desertmoon/ui/home/AdapterHomeOffers.java
@@ -0,0 +1,79 @@
+package com.desertmoon.ui.home;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.desertmoon.R;
+import com.smarteist.autoimageslider.SliderViewAdapter;
+
+public class AdapterHomeOffers extends SliderViewAdapter<AdapterHomeOffers.SliderAdapterVH> {
+
+    private Context context;
+
+    public AdapterHomeOffers(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public SliderAdapterVH onCreateViewHolder(ViewGroup parent) {
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_home_offers, null);
+        return new SliderAdapterVH(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(SliderAdapterVH viewHolder, int position) {
+       // viewHolder.textViewDescription.setText("This is slider item " + position);
+
+        switch (position) {
+            case 0:
+                Glide.with(viewHolder.itemView)
+                        .load(R.drawable.ic_realistic_food_vector_10)
+                        .into(viewHolder.imageViewBackground);
+                break;
+            case 1:
+                Glide.with(viewHolder.itemView)
+                        .load(R.drawable.ic_realistic_food_vector_11)
+
+                        .into(viewHolder.imageViewBackground);
+                break;
+            case 2:
+                Glide.with(viewHolder.itemView)
+                        .load(R.drawable.ic_donut_bar_vintage_sign)
+                        .into(viewHolder.imageViewBackground);
+                break;
+            default:
+                Glide.with(viewHolder.itemView)
+                        .load(R.drawable.four)
+                        .into(viewHolder.imageViewBackground);
+                break;
+
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        //slider view count could be dynamic size
+        return 4;
+    }
+
+    class SliderAdapterVH extends SliderViewAdapter.ViewHolder {
+
+        View itemView;
+        ImageView imageViewBackground;
+        TextView textViewDescription;
+
+        public SliderAdapterVH(View itemView) {
+            super(itemView);
+            imageViewBackground = itemView.findViewById(R.id.iv_auto_image_slider);
+            textViewDescription = itemView.findViewById(R.id.tv_auto_image_slider);
+            this.itemView = itemView;
+        }
+    }
+}
---
+++ app/src/main/java/com/desertmoon/ui/home/HomeFragment.java
@@ -0,0 +1,47 @@
+package com.desertmoon.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.desertmoon.R;
+import com.smarteist.autoimageslider.IndicatorAnimations;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        homeViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+
+            }
+        });
+
+
+        SliderView sliderView=(SliderView)root.findViewById(R.id.cardView) ;
+        sliderView.setSliderAdapter(new AdapterHomeOffers(getActivity()));
+
+       // sliderView.startAutoCycle();
+        sliderView.setIndicatorAnimation(IndicatorAnimations.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+
+        return root;
+    }
+}
