982e52b6894bd5660e4a5f34561e31df4240cb31
DeepakPawarDev <pawadeepakv@gmail.com>
2020-03-03 00:45:49 +0530

    d

---
+++ app/src/main/java/com/desertmoon/ui/orders/AdapterOrderHistory.java
@@ -0,0 +1,79 @@
+package com.desertmoon.ui.orders;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.desertmoon.R;
+import com.desertmoon.common.Menus;
+
+import java.util.List;
+
+public class AdapterOrderHistory extends RecyclerView.Adapter<com.desertmoon.ui.orders.AdapterOrderHistory.MyViewHolder> {
+
+
+    private static final String TAG = "AdapterOrderHistory";
+    private FragmentActivity context;
+    private List<Menus> listEditText;
+    private LayoutInflater mInflater;
+
+
+
+
+    public AdapterOrderHistory(FragmentActivity context, List<Menus> listEditText) {
+        this.context = context;
+        this.listEditText = listEditText;
+        this.mInflater = LayoutInflater.from(context);
+
+
+    }
+
+    @NonNull
+    @Override
+    public com.desertmoon.ui.orders.AdapterOrderHistory.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view =       mInflater.inflate  ( R.layout.row_order_history, viewGroup, false);
+
+        com.desertmoon.ui.orders.AdapterOrderHistory.MyViewHolder holder = new com.desertmoon.ui.orders.AdapterOrderHistory.MyViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull com.desertmoon.ui.orders.AdapterOrderHistory.MyViewHolder myViewHolder, int i) {
+
+        Menus beneficiary = listEditText.get(i);
+        myViewHolder.setData(beneficiary);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listEditText.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+
+
+
+        public MyViewHolder(final View itemView) {
+            super(itemView);
+
+
+        }
+
+        public void setData(final Menus beneficiary) {
+
+
+
+            }
+
+
+        }
+
+
+    }
+
--- app/src/main/java/com/desertmoon/ui/orders/OrdersFragment.java
+++ app/src/main/java/com/desertmoon/ui/orders/OrdersFragment.java
@@ -2,27 +2,37 @@ package com.desertmoon.ui.orders;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.desertmoon.R;
+import com.desertmoon.common.Menus;
+import com.desertmoon.databinding.FragmentOrdersBinding;
+
+import java.util.ArrayList;
 
 public class OrdersFragment extends Fragment {
 
     private OrdersViewModel ordersViewModel;
+    FragmentOrdersBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+        binding= DataBindingUtil.inflate(inflater,R.layout.fragment_orders, container, false);
         ordersViewModel =
                 ViewModelProviders.of(this).get(OrdersViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_orders, container, false);
+        View root = binding.getRoot();
         final TextView textView = root.findViewById(R.id.text_gallery);
         ordersViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -30,6 +40,30 @@ public class OrdersFragment extends Fragment {
                // textView.setText(s);
             }
         });
+        initialize();
+
         return root;
+    }
+
+    private void initialize() {
+        ArrayList<Menus> menusArrayList=new ArrayList<>();
+
+        Menus menu=new Menus();
+
+        menusArrayList.add(menu);
+        menusArrayList.add(menu);
+        menusArrayList.add(menu);
+        menusArrayList.add(menu);
+        menusArrayList.add(menu);
+        menusArrayList.add(menu); menusArrayList.add(menu);
+        menusArrayList.add(menu);
+
+        AdapterOrderHistory adapterOrderHistory=new AdapterOrderHistory(getActivity(),menusArrayList);
+        binding.setAdapter(adapterOrderHistory);
+
+
+
+
+
     }
 }
